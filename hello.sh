df5ebc0c6f4e438f5d3b7cd001479d756630b4cc
SumaiyyaInamdar <169063587+SumaiyyaInamdar@users.noreply.github.com>
2025-07-11 21:18:19 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -3,5 +3,6 @@ apt install update -y
 apt install apache2 -y
 systemctl start apache2
 systemctl enable apache2
+systemctl restart apache2
 systemctl status apache2
 echo "Welcome to my first project" >> /var/www/html/.
